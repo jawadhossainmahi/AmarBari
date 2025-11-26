dc1beaa61772f69638f2d7ebedf79022fa8761bd
Jawad Hossain Mahi <jawadhossainmahi@gmail.com>
2025-11-27 02:55:09 +0600

    first commit

---
+++ src/main/java/com/mycompany/varamanagementsystem/VaraManagementSystem.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.varamanagementsystem;
+
+import database.MySqlDb;
+
+/**
+ *
+ * @author Mahi
+ */
+public class VaraManagementSystem {
+
+    public static void main(String[] args) {
+        MySqlDb db = new MySqlDb();
+
+        System.out.println("Hello World!");
+    }
+}
