bf6ef2436fcabda425c47a783754d92e7dcb4a27
Jawad Hossain Mahi <jawadhossainmahi@gmail.com>
2025-11-27 16:00:49 +0600

    mvc pattern

--- src/main/java/Models/Users.java
+++ src/main/java/Models/Users.java
@@ -1,12 +1,95 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+package Models;
 
-/**
- *
- * @author Mahi
- */
 public class Users {
-    
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private String userType; // 0 = admin, 1 = owner, 2 = customer
+    private String createdAt;
+
+    // Empty constructor
+    public Users() {
+    }
+
+    // Constructor with all fields (except ID if auto-increment)
+    public Users(String firstName, String lastName, String username, String password, String userType) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.userType = userType;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    // Optional: override toString() for debugging
+    @Override
+    public String toString() {
+        return "UserModel{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", username='" + username + '\'' +
+                ", userType='" + userType + '\'' +
+                ", createdAt='" + createdAt + '\'' +
+                '}';
+    }
 }
--- src/main/java/view/VaraManagementSystem.java
+++ src/main/java/view/VaraManagementSystem.java
@@ -1,9 +1,9 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-package com.mycompany.varamanagementsystem;
+package view;
 
-import database.MySqlDb;
+import Database.MySqlDb;
 
 /**
  *
